89e6bef7d00fcca7d9128a9163baa3d1dba128a1
Fay Montage <accounts@faymontage.com>
2017-04-05 14:31:01 +0200

    Only log ERROR and above from Signal

--- src/main/java/com/bakkenbaeck/token/headless/HeadlessSignalProtocolLogger.java
+++ src/main/java/com/bakkenbaeck/token/headless/HeadlessSignalProtocolLogger.java
@@ -5,6 +5,8 @@ import org.whispersystems.libsignal.logging.SignalProtocolLogger;
 public class HeadlessSignalProtocolLogger implements SignalProtocolLogger {
     @Override
     public void log(int priority, String tag, String message) {
-        System.out.println(tag+": "+message);
+        if (priority >= SignalProtocolLogger.ERROR) {
+            System.out.println(tag + "-" + String.valueOf(priority) + ": " + message);
+        }
     }
 }
