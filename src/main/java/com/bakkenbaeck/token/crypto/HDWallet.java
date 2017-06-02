2c25d37c69fec18500ec9eec2081b45d1511dfb3
Tristan King <tristan@bakkenbaeck.no>
2017-06-02 13:19:29 +0200

    Change derivation path and validate seed and username

--- src/main/java/com/bakkenbaeck/token/crypto/HDWallet.java
+++ src/main/java/com/bakkenbaeck/token/crypto/HDWallet.java
@@ -5,10 +5,13 @@ package com.bakkenbaeck.token.crypto;
 
 //import com.bakkenbaeck.token.R;
 
+import com.tokenbrowser.crypto.hdshim.EthereumKeyChainGroup;
 import com.bakkenbaeck.token.crypto.util.TypeConverter;
+import com.tokenbrowser.exception.InvalidMasterSeedException;
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.crypto.DeterministicKey;
 import org.bitcoinj.crypto.MnemonicCode;
+import org.bitcoinj.crypto.MnemonicException;
 import org.bitcoinj.wallet.DeterministicSeed;
 import org.bitcoinj.wallet.KeyChain;
 import org.bitcoinj.wallet.UnreadableWalletException;
@@ -34,32 +37,16 @@ public class HDWallet {
     private static final String MASTER_SEED = "ms";
     //private SharedPreferences prefs;
     private ECKey identityKey;
-    private ECKey receivingKey;
+    private ECKey paymentKey;
     private String masterSeed;
 
     public HDWallet init(String seed) {
-        initPreferences();
-        initWordList();
         initWallet(seed);
         return this;
     }
 
-    private void initPreferences() {
-        //this.prefs = BaseApplication.get().getSharedPreferences("wa", Context.MODE_PRIVATE);
-    }
-
-    private void initWordList() {
-        try {
-            InputStream targetStream = this.getClass().getResourceAsStream("bip39_wordlist.txt");
-            MnemonicCode.INSTANCE = new MnemonicCode(targetStream, null);
-        } catch (final IOException e) {
-            //LogUtil.e(getClass(), "Wordlist not loaded.");
-            throw new RuntimeException(e);
-        }
-    }
-
     private HDWallet initWallet(String seed) {
-        this.masterSeed = seed;//null;// readMasterSeedFromStorage();
+        this.masterSeed = seed;
         final Wallet wallet = this.masterSeed == null
                 ? generateNewWallet()
                 : initFromMasterSeed(masterSeed);
@@ -74,22 +61,29 @@ public class HDWallet {
         final DeterministicSeed seed = wallet.getKeyChainSeed();
         this.masterSeed = seedToString(seed);
 
-        //HACK
-        //this.prefs.edit()
-        //        .putString(MASTER_SEED, this.masterSeed)
-        //        .apply();
-
         return wallet;
     }
 
     private Wallet initFromMasterSeed(final String masterSeed) {
         try {
-            final NetworkParameters networkParameters = NetworkParameters.fromID(NetworkParameters.ID_MAINNET);
-            final DeterministicSeed seed = new DeterministicSeed(masterSeed, null, "", 0);
-            return Wallet.fromSeed(networkParameters, seed);
-        } catch (final UnreadableWalletException e) {
+            final DeterministicSeed seed = getSeed(masterSeed);
+            seed.check();
+            return constructFromSeed(seed);
+        } catch (final UnreadableWalletException | MnemonicException e) {
             throw new RuntimeException("Unable to create wallet. Seed is invalid");
         }
+    }
+
+    private Wallet constructFromSeed(final DeterministicSeed seed) {
+        return new Wallet(getNetworkParameters(),  new EthereumKeyChainGroup(getNetworkParameters(), seed));
+    }
+
+    private DeterministicSeed getSeed(final String masterSeed) throws UnreadableWalletException {
+        return new DeterministicSeed(masterSeed, null, "", 0);
+    }
+
+    private NetworkParameters getNetworkParameters() {
+        return NetworkParameters.fromID(NetworkParameters.ID_MAINNET);
     }
 
     private void deriveKeysFromWallet(final Wallet wallet) {
@@ -106,7 +100,7 @@ public class HDWallet {
     }
 
     private void deriveReceivingKey(final Wallet wallet) throws IOException, UnreadableWalletException {
-        this.receivingKey = deriveKeyFromWallet(wallet, 0, KeyChain.KeyPurpose.RECEIVE_FUNDS);
+        this.paymentKey = deriveKeyFromWallet(wallet, 0, KeyChain.KeyPurpose.RECEIVE_FUNDS);
     }
 
     private ECKey deriveKeyFromWallet(final Wallet wallet, final int iteration, final KeyChain.KeyPurpose keyPurpose) throws UnreadableWalletException, IOException {
@@ -114,6 +108,11 @@ public class HDWallet {
         for (int i = 0; i <= iteration; i++) {
             key = wallet.freshKey(keyPurpose);
         }
+
+        if (key == null) {
+            throw new IOException("Unable to derive key");
+        }
+
         return ECKey.fromPrivate(key.getPrivKey());
     }
 
@@ -124,7 +123,7 @@ public class HDWallet {
     public String signTransaction(final String data) {
         try {
             final byte[] transactionBytes = TypeConverter.StringHexToByteArray(data);
-            return sign(transactionBytes, this.receivingKey);
+            return sign(transactionBytes, this.paymentKey);
         } catch (final Exception e) {
             System.err.println("Unable to sign transaction. " + e);
             return null;
@@ -149,27 +148,23 @@ public class HDWallet {
         return Hex.toHexString(this.identityKey.getPubKey());
     }
 
-    public String getAddress() {
-        if(identityKey != null) {
+    public String getOwnerAddress() {
+        if (identityKey != null) {
             return TypeConverter.toJsonHex(this.identityKey.getAddress());
         }
         return null;
     }
 
-    public String getWalletAddress() {
-        if(this.receivingKey != null) {
-            return TypeConverter.toJsonHex(this.receivingKey.getAddress());
+    public String getPaymentAddress() {
+        if (this.paymentKey != null) {
+            return TypeConverter.toJsonHex(this.paymentKey.getAddress());
         }
         return null;
     }
 
     @Override
     public String toString() {
-        return "Private: " + getPrivateKey() + "\nPublic: " + getPublicKey() + "\nAddress: " + getAddress();
-    }
-
-    private String readMasterSeedFromStorage() {
-        return "";//this.prefs.getString(MASTER_SEED, null);
+        return "Private: " + getPrivateKey() + "\nPublic: " + getPublicKey() + "\nAddress: " + getOwnerAddress();
     }
 
     private String seedToString(final DeterministicSeed seed) {
